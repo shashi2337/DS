86ac30416ca5c575273b9bd4da546c7fadf35a31
shashi kant awasthi <shashikant.awasthi@gmail.com>
2022-01-15 00:54:29 +0530

    CoinChange

--- Medium/src/dp/CoinChange.java
+++ Medium/src/dp/CoinChange.java
@@ -1,15 +1,32 @@
 package dp;
 
 public class CoinChange {
-	public int change(int amount, int[] coins) {
-	    int[] dp = new int[amount + 1];
-	    dp[0] = 1;
-
-	    for (int coin : coins) {
-	      for (int x = coin; x < amount + 1; ++x) {
-	        dp[x] += dp[x - coin];
-	      }
-	    }
-	    return dp[amount];
-	  }
+	public int coinChange(int[] coins, int amount) {
+        return coinChange(coins, amount, new int[amount + 1]);
+    }
+    
+    private int coinChange(int[] coins, int amount, int[] dp) {
+        if (amount == 0) {
+            return 0;
+        }
+        
+        if (amount < 0) {
+            return -1;
+        }
+        
+        if (dp[amount - 1] != 0) {
+            return dp[amount - 1];
+        }
+        
+        int min = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int curr = coinChange(coins, amount - coin, dp);
+            if (curr >= 0) {
+                min = Math.min(min, 1 + curr);
+            }
+        }
+        
+        dp[amount - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+        return dp[amount - 1];
+    }
 }
