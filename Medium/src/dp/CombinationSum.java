c46ba7e3ba918e1103394413dcc18cdee76bf03c
shashi kant awasthi <shashikant.awasthi@gmail.com>
2022-01-15 16:37:38 +0530

    CombinationSum

---
+++ Medium/src/dp/CombinationSum.java
@@ -0,0 +1,29 @@
+package dp;
+
+public class CombinationSum {
+	
+	public int combinationSum4(int[] nums, int target) {
+        return combination(nums, target, new HashMap<Integer, Integer>());
+    }
+    
+    private int combination(int[] nums, int remain, Map<Integer, Integer> memo) {
+        if (remain == 0) {
+            return 1;
+        }
+        
+        if (memo.containsKey(remain)) {
+            return memo.get(remain);
+        }
+        
+        int result = 0;
+        for (int num : nums) {
+            if (remain - num >= 0) {
+                result += combination(nums, remain - num, memo);
+            }
+        }
+        
+        memo.put(remain, result);
+        return result;
+    }
+
+}
